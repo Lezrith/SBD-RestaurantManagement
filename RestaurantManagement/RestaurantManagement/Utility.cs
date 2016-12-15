bd05ad8754a2aab58ebbf34ded085b830331ce4f
Lezrith <krzysztof.g.tomczak@student.put.poznan.pl>
2016-12-16 00:28:41 +0100

    Added and designed LoginWindow.
    Check if fields are empty.
    Other functionality not yet implemented.
    Added utility class and more fonts.

---
+++ RestaurantManagement/RestaurantManagement/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement
+{
+    internal static class Utility
+    {
+        static public bool IsAssemblyDebugBuild()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(Utility));
+            return assembly.GetCustomAttributes(false).OfType<DebuggableAttribute>().Select(da => da.IsJITTrackingEnabled).FirstOrDefault();
+        }
+
+        static public string HashString(string s)
+        {
+            byte[] salt;
+            byte[] hashBytes;
+            byte[] hash;
+            using (var rNGCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                rNGCryptoServiceProvider.GetBytes(salt = new byte[16]);
+                using (var pbkdf2 = new Rfc2898DeriveBytes(s, salt, 1000))
+                {
+                    hash = pbkdf2.GetBytes(20);
+                    hashBytes = new byte[36];
+                    Array.Copy(salt, 0, hashBytes, 0, 16);
+                    Array.Copy(hash, 0, hashBytes, 16, 20);
+                }
+            }
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
---
+++ RestaurantManagement/RestaurantManagement/Views/LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestaurantManagement
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : MetroWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            if (Utility.IsAssemblyDebugBuild()) warningLabel.Content = "Aplikacja w trybie deweloperskim!";
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = username = usernameTextBox.Text;
+            string password = passwordBox.Password;
+            if (username == string.Empty) usernameTextBox.BorderBrush = Brushes.Red;
+            if (password == string.Empty) passwordBox.BorderBrush = Brushes.Red;
+            if (username != string.Empty && password != string.Empty)
+            {
+                //try to login user
+            }
+        }
+
+        private void usernameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (usernameTextBox.BorderBrush == Brushes.Red) usernameTextBox.ClearValue(BorderBrushProperty);
+        }
+
+        private void passwordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox.BorderBrush == System.Windows.Media.Brushes.Red) passwordBox.ClearValue(BorderBrushProperty);
+        }
+    }
+}
