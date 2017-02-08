a6eae5f7d3a4cdcd8e8b41bb3a1643c4cfecd643
Lezrith <krzysztof.g.tomczak@student.put.poznan.pl>
2017-02-08 17:27:09 +0100

    Suppliers can now be added and edited.

--- RestaurantManagement/RestaurantManagement/Views/SupplierAddEditWindow.xaml.cs
+++ RestaurantManagement/RestaurantManagement/Views/SupplierAddEditWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using RestaurantManagement.Model;
+using System.Data.Entity;
 
 namespace RestaurantManagement.Views
 {
@@ -21,15 +22,66 @@ namespace RestaurantManagement.Views
     /// </summary>
     public partial class SupplierAddEditWindow : MetroWindow
     {
+        private Supplier supplier;
+        private Address address;
+
         public SupplierAddEditWindow(string title, Supplier supplier)
         {
             InitializeComponent();
             this.Title = title;
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            if (supplier == null)
+            {
+                this.supplier = new Supplier();
+                this.address = new Address();
+            }
+            else
+            {
+                this.supplier = supplier;
+                using (var context = new RestaurantDBEntities())
+                {
+                    this.address = context.Addresses.ToList().Find(address => address.Supplier_Name == this.supplier.Name);
+                }
+                nameTextBox.IsReadOnly = true;
+            }
+            grid1.DataContext = this.supplier;
+            grid2.DataContext = this.address;
         }
 
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void confirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new RestaurantDBEntities())
+            {
+                if (!nameTextBox.IsReadOnly)
+                {
+                    context.Addresses.Add(this.address);
+                    context.SaveChanges();
+                    this.supplier.Address = this.address;
+                    this.address.Supplier = this.supplier;
+                    context.Addresses.Attach(this.address);
+                    context.Entry(this.address).State = EntityState.Modified;
+                    context.Suppliers.Add(this.supplier);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    context.Suppliers.Attach(this.supplier);
+                    context.Entry(this.supplier).State = EntityState.Modified;
+                    context.Addresses.Attach(this.address);
+                    context.Entry(this.address).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+            }
+            this.Close();
         }
     }
 }
--- RestaurantManagement/RestaurantManagement/Views/SuppliersView.xaml.cs
+++ RestaurantManagement/RestaurantManagement/Views/SuppliersView.xaml.cs
@@ -24,6 +24,7 @@ namespace RestaurantManagement.Views
         public SuppliersView()
         {
             InitializeComponent();
+            PopulateSuppliersDataGrid();
         }
 
         private void addButton_Click(object sender, RoutedEventArgs e)
@@ -31,18 +32,27 @@ namespace RestaurantManagement.Views
             OpenSupplierAddEditWindow("Dodaj dostawcę", null);
         }
 
-        private void OpenSupplierAddEditWindow(string title, Supplier order)
+        private void OpenSupplierAddEditWindow(string title, Supplier supplier)
         {
-            var addEditWindow = new SupplierAddEditWindow(title, order);
+            var addEditWindow = new SupplierAddEditWindow(title, supplier);
             addEditWindow.ShowDialog();
+            PopulateSuppliersDataGrid();
         }
 
         private void editButton_Click(object sender, RoutedEventArgs e)
         {
-            var selectedOrders = suppliersDataGrid.SelectedItems;
-            if (selectedOrders.Count == 1)
+            var selectedSuppliers = suppliersDataGrid.SelectedItems;
+            if (selectedSuppliers.Count == 1)
             {
-                OpenSupplierAddEditWindow("Edytuj dostawcę", null);
+                OpenSupplierAddEditWindow("Edytuj dostawcę", (Supplier)selectedSuppliers[0]);
+            }
+        }
+
+        private void PopulateSuppliersDataGrid()
+        {
+            using (var context = new RestaurantDBEntities())
+            {
+                suppliersDataGrid.ItemsSource = context.Suppliers.ToList();
             }
         }
     }
